098f9f46a2ce8d8533b52610140f5243c3c347b4
Hjiaxing97 <43169035+hovercao@users.noreply.github.com>
2022-09-25 13:13:35 +0800

    feat:获取元素和组件的DOM元素

--- src/01_数据不可变性/App.jsx
+++ src/01_数据不可变性/App.jsx
@@ -1,7 +1,35 @@
-import React, {PureComponent} from "react";
+import React, { PureComponent } from "react";
 
 export default class App extends PureComponent {
-    render() {
-        return <>App</>
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          name: "123",
+        },
+      ],
+    };
+  }
+
+  addNewBook() {
+    const param = { name: "222" };
+    const newBooks = [...this.state.books];
+    newBooks.push({ ...param });
+    this.setState({
+      books: newBooks,
+    });
+  }
+
+  render() {
+    const { books } = this.state;
+    return (
+      <>
+        {books.map((item, index) => {
+          return <h2 key={index}>{item.name}</h2>;
+        })}
+        <button onClick={() => this.addNewBook()}>+1</button>
+      </>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './01_数据不可变性/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './01_数据不可变性/App';
+import App from "./02_获取DOM的ref/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-
