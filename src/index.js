42fe1e5c381e2b50eac8b2463813ca6c7d2456b7
hovercao <17388766232@163.com>
2022-10-08 18:15:40 +0800

    feat:初始化

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@
  * @Author: hovercao 17388766232@163.com
  * @Date: 2022-09-26 08:05:27
  * @LastEditors: hovercao 17388766232@163.com
- * @LastEditTime: 2022-10-08 17:33:25
+ * @LastEditTime: 2022-10-08 18:14:11
  * @FilePath: \AgainReact\src\index.js
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+root.render(<App />);
