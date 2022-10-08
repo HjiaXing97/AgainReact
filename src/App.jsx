28bc753c2740372ee1f7f1af5a3efc1f435df747
hovercao <17388766232@163.com>
2022-10-08 17:27:08 +0800

    feat:初始化

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: hovercao 17388766232@163.com
+ * @Date: 2022-10-08 17:26:05
+ * @LastEditors: hovercao 17388766232@163.com
+ * @LastEditTime: 2022-10-08 17:26:11
+ * @FilePath: \AgainReact\src\App.jsx
+ */
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
+/*
+ * @Author: hovercao 17388766232@163.com
+ * @Date: 2022-09-26 08:05:27
+ * @LastEditors: hovercao 17388766232@163.com
+ * @LastEditTime: 2022-10-08 17:26:37
+ * @FilePath: \AgainReact\src\index.js
+ */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./store";
 
-// import App from './01_数据不可变性/App';
-// import App from "./02_获取DOM的ref/App";
-// import App from "./03_高阶组件/App";
-// import App from "./04_css_in_js/App";
-// import App from "./05_redux使用/App";
-import App from "./06_redux异步操作/App";
+import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
