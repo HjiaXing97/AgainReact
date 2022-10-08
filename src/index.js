0d7c11acd04bff28c4f8e1924f034bb1e5dcc805
hovercao <17388766232@163.com>
2022-10-08 17:38:40 +0800

    feat:安装插件

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@
  * @Author: hovercao 17388766232@163.com
  * @Date: 2022-09-26 08:05:27
  * @LastEditors: hovercao 17388766232@163.com
- * @LastEditTime: 2022-10-08 17:26:37
+ * @LastEditTime: 2022-10-08 17:33:25
  * @FilePath: \AgainReact\src\index.js
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
