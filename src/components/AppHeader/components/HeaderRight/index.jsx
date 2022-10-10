7657a75fe1d693d59e1c5ae49e129fb2952aa88b
hovercao <17388766232@163.com>
2022-10-10 17:51:59 +0800

    feat:Header组件右侧弹窗

--- src/components/AppHeader/components/HeaderRight/index.jsx
+++ src/components/AppHeader/components/HeaderRight/index.jsx
@@ -1,14 +1,32 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import {
   BthList,
   BtnItem,
   Icon,
+  Panel,
   Profile,
   RightWrapper,
   UserIcon,
 } from "./style";
 
 const HeaderRight = memo(() => {
+  const [showPanel, setShowPanel] = useState(false);
+
+  function profileClickHandle(e) {
+    setShowPanel(true);
+  }
+  function closePanel() {
+    setShowPanel(false);
+  }
+
+  /** 监听全局的点击事件，关闭 Panel*/
+  useEffect(() => {
+    window.addEventListener("click", closePanel, true);
+    //取消监听
+    return () => {
+      window.removeEventListener("click", closePanel);
+    };
+  }, []);
   return (
     <RightWrapper>
       <BthList>
@@ -18,8 +36,23 @@ const HeaderRight = memo(() => {
           <Icon className="iconfont">&#xe62d;</Icon>
         </BtnItem>
         <Profile>
-          <Icon className="iconfont">&#xe643;</Icon>
+          <Icon className="iconfont" onClick={(e) => profileClickHandle(e)}>
+            &#xe643;
+          </Icon>
           <UserIcon className="iconfont">&#xe787;</UserIcon>
+          {showPanel && (
+            <Panel>
+              <div className="top">
+                <span>注册</span>
+                <span>登录</span>
+              </div>
+              <div className="bottom">
+                <span>出租房源</span>
+                <span>开展体验</span>
+                <span>帮助</span>
+              </div>
+            </Panel>
+          )}
         </Profile>
       </BthList>
     </RightWrapper>
