4b3e05e77cefc19e51ab0ea2a6c4a9a1858335f5
hovercao <17388766232@163.com>
2022-10-10 10:24:11 +0800

    feat:初始化

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 /*
  * @Author: hovercao 17388766232@163.com
- * @Date: 2022-09-26 08:05:27
+ * @Date: 2022-10-10 10:20:38
  * @LastEditors: hovercao 17388766232@163.com
- * @LastEditTime: 2022-10-10 09:03:34
+ * @LastEditTime: 2022-10-10 10:23:40
  * @FilePath: \AgainReact\src\index.js
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./06_redux-hooks/store";
-
-// import App from "./01_useContext/App";
-// import App from "./02_useReducer/App";
-// import App from "./03_useCallback/App";
-// import App from "./04_useMemo/App";
-// import App from "./05_自定义hooke/App";
-import App from "./06_redux-hooks/App";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <UserContext.Provider value={{ name: "hjx", level: 99 }}>
-  //   <ThemeContext.Provider value={{ color: "red" }}>
-  //     <App />
-  //   </ThemeContext.Provider>
-  // </UserContext.Provider>
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
