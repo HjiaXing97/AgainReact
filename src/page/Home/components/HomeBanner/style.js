8db5f965d1a4a6956aa5751981d9d287cece6629
hovercao <17388766232@163.com>
2022-10-10 18:09:06 +0800

    feat:home头部引入图片

---
+++ src/page/Home/components/HomeBanner/style.js
@@ -0,0 +1,7 @@
+import backImg from "src/assets/image/aad16e09-d997-4272-a473-9c05f1779713.jpg";
+import styled from "styled-components";
+
+export const BannerWrapper = styled.div`
+  background: url(${backImg}) center/cover;
+  height: 529px;
+`;
--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,20 +1,12 @@
-/*
- * @Author       : hovercao 17388766232@163.com
- * @Date         : 2022-10-10 14:09:05
- * @LastEditors  : hovercao 17388766232@163.com
- * @LastEditTime : 2022-10-10 14:51:27
- * @FilePath     : \AgainReact\src\page\Home\index.jsx
- */
-import { memo, useEffect } from "react";
-import request from "src/services";
-
+import { memo } from "react";
+import HomeBanner from "./components/HomeBanner";
+import { HomeWrapper } from "./style";
 const Home = memo(() => {
-  useEffect(() => {
-    request.get({ url: "/home/highscore" }).then((res) => {
-      console.log(res);
-    });
-  }, []);
-  return <div>Home</div>;
+  return (
+    <HomeWrapper>
+      <HomeBanner></HomeBanner>
+    </HomeWrapper>
+  );
 });
 
 export default Home;
