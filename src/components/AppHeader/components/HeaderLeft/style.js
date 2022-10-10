fbead031ebd0a808590b2bd9419787140e3c3264
hovercao <17388766232@163.com>
2022-10-10 16:25:25 +0800

    feat:配置主题

--- src/components/AppHeader/components/HeaderLeft/style.js
+++ src/components/AppHeader/components/HeaderLeft/style.js
@@ -2,11 +2,16 @@ import styled from "styled-components";
 
 export const LeftWrapper = styled.div`
   flex: 1;
+  padding-left: 20px;
 `;
 
 export const Icon = styled.i`
   &.iconfont {
-    font-size: 50px;
-    color: #ff385c;
+    font-size: 45px;
+    /* color: var(--primary-color);   //使用css变量 */
+    color: ${(props) =>
+      props.theme.color
+        .primaryColor}; //使用styled-components中的theme方式存入变量
+    cursor: pointer;
   }
 `;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author       : hovercao 17388766232@163.com
  * @Date         : 2022-10-10 10:20:38
  * @LastEditors  : hovercao 17388766232@163.com
- * @LastEditTime : 2022-10-10 14:19:13
+ * @LastEditTime : 2022-10-10 16:18:53
  * @FilePath     : \AgainReact\src\index.js
  */
 import "assets/css/index.less";
@@ -11,13 +11,17 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "src/App";
+import { theme } from "src/assets/theme";
+import { ThemeProvider } from "styled-components";
 import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </HashRouter>
 );
