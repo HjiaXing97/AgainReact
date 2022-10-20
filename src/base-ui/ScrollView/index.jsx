c0784728d4efa3d7b3a11cdfab2ad2c8ca60b7a6
hovercao <17388766232@163.com>
2022-10-20 19:20:04 +0800

    feat；完成tab滚动及添加动画

--- src/base-ui/ScrollView/index.jsx
+++ src/base-ui/ScrollView/index.jsx
@@ -1,33 +1,50 @@
 import React, { memo, useEffect, useRef, useState } from "react";
-import { ViewWrapper, ScrollCount } from "./style";
+import { ViewWrapper, ScrollCount, ScrollBut } from "./style";
 
 const ScrollView = memo((props) => {
   const scrollRef = useRef();
 
-  const [showRightBtn, setShowRightBtn] = useState(false);
-  const [posIndex, setPosIndex] = useState(0);
+  const [showRightBtn, setShowRightBtn] = useState(false); //是否展示右侧按钮
+  const [showLeft, setShowLeft] = useState(false);
+  const [posIndex, setPosIndex] = useState(0); //滚动index
+  const [totalDance, setTotalDance] = useState(0);
 
   useEffect(() => {
     const scrollWidth = scrollRef.current.scrollWidth; //元素的实际宽度
     const clientWidth = scrollRef.current.clientWidth; //可视区域的宽度
     const totalDance = scrollWidth - clientWidth;
+    setTotalDance(totalDance);
     setShowRightBtn(totalDance > 0);
   }, [props.children]);
 
-  function rightBtnHandle() {
-    const newIndex = posIndex + 1;
+  function controlClickHandle(isRight = true) {
+    const newIndex = isRight ? posIndex + 1 : posIndex - 1;
     const childEl = scrollRef.current.children[newIndex];
-    scrollRef.current.style.transform = `translate(-${childEl.offsetLeft}px)`;
+    const newOffsetLeft = childEl.offsetLeft;
+    scrollRef.current.style.transform = `translate(-${newOffsetLeft}px)`;
+    setPosIndex(newIndex);
+    setShowRightBtn(totalDance > newOffsetLeft);
+    setShowLeft(newOffsetLeft > 0);
   }
+
   return (
     <ViewWrapper>
-      <div className="left-but">left-but</div>
-      {showRightBtn && (
-        <div className="right-but" onClick={rightBtnHandle}>
-          right-but
-        </div>
+      {showLeft && (
+        <ScrollBut
+          className="left-but but"
+          onClick={() => controlClickHandle(false)}
+        >
+          <i className="iconfont">&#xe602;</i>
+        </ScrollBut>
       )}
-      <ScrollCount ref={scrollRef}>{props.children}</ScrollCount>
+      {showRightBtn && (
+        <ScrollBut className="right-but but" onClick={controlClickHandle}>
+          <i className="iconfont">&#xe60c;</i>
+        </ScrollBut>
+      )}
+      <div className="scroll">
+        <ScrollCount ref={scrollRef}>{props.children}</ScrollCount>
+      </div>
     </ViewWrapper>
   );
 });
--- src/base-ui/ScrollView/style.js
+++ src/base-ui/ScrollView/style.js
@@ -1,10 +1,42 @@
 import styled from "styled-components";
 
 export const ViewWrapper = styled.div`
-  overflow: hidden;
   position: relative;
+  .scroll {
+    overflow: hidden;
+  }
+  .iconfont {
+    cursor: pointer;
+    font-size: 20px;
+  }
 `;
 
 export const ScrollCount = styled.div`
   display: flex;
+  transition: transform 300ms ease;
+`;
+
+export const ScrollBut = styled.div`
+  position: absolute;
+  height: 28px;
+  width: 28px;
+  top: 50%;
+  transform: translate(0, -50%);
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 20;
+  border-radius: 50%;
+  text-align: center;
+  border-width: 2px;
+  box-shadow: 0 1px 1px 1px rgba(0, 0, 0, 0.14);
+  &.right-but {
+    right: 0;
+    transform: translate(50%, -50%);
+  }
+  &.left-but {
+    left: 0;
+    transform: translate(-50%, -50%);
+  }
 `;
